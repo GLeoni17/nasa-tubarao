cd7d6639bd492edbb63213725e025a10e9084d03
Ana Beatriz Zerati <anabeatrizsilvazerati28@gmail.com>
2025-10-04 23:36:07 -0300

    comentarios

--- llm/llm.py
+++ llm/llm.py
@@ -1,6 +1,8 @@
 from huggingface_hub import InferenceClient
 import pandas as pd
 
+# dados fictícios de 1 tubarão
+# sensor + satélites NASA + forecasting
 data = pd.DataFrame([{
     'shark_id': 1,
     'lat': -15.2,
@@ -12,29 +14,21 @@ data = pd.DataFrame([{
     'feeding_prob': 0.78
 }])
 
-# Hugging Face Inference API
 client = InferenceClient()
 
 MODEL_NAME = "deepseek-ai/DeepSeek-V3-0324"
 
 # gerar texto educativo resumido
 def generate_educational_text(row):
-    prompt = (
-        f"Você é um educador de biologia marinha. Resuma em 3-4 frases para o público geral os hábitos de caça de um tubarão "
-        f"com os seguintes dados: profundidade {row['depth']} m, velocidade {row['speed']} m/s, "
-        f"temperatura da água {row['sea_surface_temp']}°C, clorofila {row['chlorophyll']} mg/m³, "
-        f"probabilidade de alimentação {row['feeding_prob']:.2f}."
-    )
-    
     completion = client.chat.completions.create(
     model="deepseek-ai/DeepSeek-V3-0324",
     messages=[
         {
             "role": "user",
             "content": f"Você é um educador de biologia marinha. Resuma em 3-4 frases para o público geral os hábitos de caça de um tubarão "
-        f"com os seguintes dados: profundidade {row['depth']} m, velocidade {row['speed']} m/s, "
-        f"temperatura da água {row['sea_surface_temp']}°C, clorofila {row['chlorophyll']} mg/m³, "
-        f"probabilidade de alimentação {row['feeding_prob']:.2f}."
+            f"com os seguintes dados: profundidade {row['depth']} m, velocidade {row['speed']} m/s, "
+            f"temperatura da água {row['sea_surface_temp']}°C, clorofila {row['chlorophyll']} mg/m³, "
+            f"probabilidade de alimentação {row['feeding_prob']:.2f}."
         }
     ],
 )
